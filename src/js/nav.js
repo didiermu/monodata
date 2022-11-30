8bdafa1b4f8adbe51e4df1f59a4c8ed8ddc44378
didier-mu <didier.m@bnn.mx>
2022-11-30 01:18:35 -0600

    update: mobile

---
+++ src/js/nav.js
@@ -0,0 +1,29 @@
+const btnRemove = () => {
+    const mediaquery = window.matchMedia("(max-width: 768px)");
+
+    const btnSearch = document.querySelector(".icon-search");
+    const headerRow = document.querySelector(".header--row");
+    const headerTwo = document.querySelector(".header--two");
+
+    // const navbarNav = document.querySelector("nav");
+    // const navbarNavDesk = document.querySelector(".header--navegacion ul");
+    // const botonHeader = document.querySelector("#link-btn-header");
+
+    if (mediaquery.matches) {
+        headerRow.append(btnSearch);
+    } else {
+        headerTwo.append(btnSearch);
+    }
+};
+
+const nav = () => {
+    btnHam = document.querySelector(".btn-ham");
+    btnHam.onclick = function () {
+        document.querySelector("body").classList.toggle("nav-on");
+        document.querySelector(".header").classList.toggle("open");
+        btnHam.classList.toggle("ham-on");
+    };
+};
+
+nav();
+btnRemove();
